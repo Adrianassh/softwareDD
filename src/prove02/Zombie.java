bc52862b6126afe4958f7a05634e4af1ef7b6aaf
Adrianassh <83506117+Adrianassh@users.noreply.github.com>
2021-05-01 18:30:34 -0500

    creacion de proyecto

---
+++ src/prove02/Zombie.java
@@ -0,0 +1,30 @@
+package prove02;
+
+import java.awt.*;
+
+public class Zombie extends Creature implements Movable, Aggressor{
+    @Override
+    Shape getShape() {
+        return null;
+    }
+
+    @Override
+    Color getColor() {
+        return null;
+    }
+
+    @Override
+    Boolean isAlive() {
+        return null;
+    }
+
+    @Override
+    public void attack(Creature target) {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
